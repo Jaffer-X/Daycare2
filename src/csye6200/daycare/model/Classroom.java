c1496c0d46a0e65ae0b7e1ed2662ee3b3123285e
Jaffer-X <sdjuxjf@gmail.com>
2019-11-13 16:42:51 -0500

    model

--- src/csye6200/daycare/model/Classroom.java
+++ src/csye6200/daycare/model/Classroom.java
@@ -1,7 +1,51 @@
 package csye6200.daycare.model;
+
+import java.sql.Timestamp;
+
 /*
  * author:jf
  */
 public class Classroom {
-
+	private int ClassroomId;
+	private int Capacity;
+	private int Remain;
+	private Timestamp Timestamp ;
+	public int getClassroomId() {
+		return ClassroomId;
+	}
+	public void setClassroomId(int classroomId) {
+		ClassroomId = classroomId;
+	}
+	public int getCapacity() {
+		return Capacity;
+	}
+	public void setCapacity(int capacity) {
+		Capacity = capacity;
+	}
+	public int getRemain() {
+		return Remain;
+	}
+	public void setRemain(int remain) {
+		Remain = remain;
+	}
+	public Timestamp getTimestamp() {
+		return Timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		Timestamp = timestamp;
+	}
+	@Override
+	public String toString() {
+		return "Classroom [ClassroomId=" + ClassroomId + ", Capacity=" + Capacity + ", Remain=" + Remain
+				+ ", Timestamp=" + Timestamp + "]";
+	}
+	public Classroom(int classroomId, int capacity, int remain, java.sql.Timestamp timestamp) {
+		super();
+		ClassroomId = classroomId;
+		Capacity = capacity;
+		Remain = remain;
+		Timestamp = timestamp;
+	}
+	
+	
 }
--- src/csye6200/daycare/model/Vaccine.java
+++ src/csye6200/daycare/model/Vaccine.java
@@ -1,7 +1,67 @@
 package csye6200.daycare.model;
+
+import java.sql.Timestamp;
+
 /*
  * author:jf
  */
 public class Vaccine {
-
+	private int VaccineId;
+	private String Name;
+	private int Dose;
+	private String Period;
+	private String Category;
+	private Timestamp Timestamp;
+	public int getVaccineId() {
+		return VaccineId;
+	}
+	public void setVaccineId(int vaccineId) {
+		VaccineId = vaccineId;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getDose() {
+		return Dose;
+	}
+	public void setDose(int dose) {
+		Dose = dose;
+	}
+	public String getPeriod() {
+		return Period;
+	}
+	public void setPeriod(String period) {
+		Period = period;
+	}
+	public String getCategory() {
+		return Category;
+	}
+	public void setCategory(String category) {
+		Category = category;
+	}
+	public Timestamp getTimestamp() {
+		return Timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		Timestamp = timestamp;
+	}
+	@Override
+	public String toString() {
+		return "Vaccine [VaccineId=" + VaccineId + ", Name=" + Name + ", Dose=" + Dose + ", Period=" + Period
+				+ ", Category=" + Category + ", Timestamp=" + Timestamp + "]";
+	}
+	public Vaccine(int vaccineId, String name, int dose, String period, String category, java.sql.Timestamp timestamp) {
+		super();
+		VaccineId = vaccineId;
+		Name = name;
+		Dose = dose;
+		Period = period;
+		Category = category;
+		Timestamp = timestamp;
+	}
+	
+	
 }
